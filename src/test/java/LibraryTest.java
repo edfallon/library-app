0037eb5a7e3c0e1d9d784698c4b782ba26d37d8d
Scott Stevenson <scotifer92@googlemail.com>
2018-05-23 14:14:59 +0100

    Library setup started

---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class LibraryTest {
+
+    Library library1;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void name() {
+    }
+}
