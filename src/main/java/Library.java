4a7ace636684feed267487be1ca1eb75f38c5f7e
Scott Stevenson <scotifer92@googlemail.com>
2018-05-23 14:05:19 +0100

    Adds Library with empty catalogue

---
+++ src/main/java/Library.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Library {
+
+    private int id;
+    private ArrayList<Book> catalogue;
+
+    public Library() {
+        this.catalogue = new ArrayList<Book>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ArrayList<Book> getCatalogue() {
+        return catalogue;
+    }
+
+    public void setCatalogue(ArrayList<Book> catalogue) {
+        this.catalogue = catalogue;
+    }
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+
+    Library library1;
+
+    @Before
+    public void setUp() throws Exception {
+        library1 = new Library();
+    }
+
+    @Test
+    public void hasEmptyListOfBooks() {
+        assertEquals(0, library1.getCatalogue().size());
+    }
+}
