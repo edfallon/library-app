c7280357b6467875b5752c50dd01a8fb407a6bbc
Scott Stevenson <scotifer92@googlemail.com>
2018-05-23 15:08:37 +0100

    Adds users to seed db

--- src/main/java/models/Seed.java
+++ src/main/java/models/Seed.java
@@ -12,10 +12,17 @@ public class Seed {
         Book book2 = new Book("The Romance of the Three Kingdoms");
         Book book3 = new Book("Hyperion");
 
+        User user1 = new User("George Martin");
+        User user2 = new User("Mary Beard");
+        User user3 = new User("Christopher Hitchens");
+
         DBHelper.save(book1);
         DBHelper.save(book2);
         DBHelper.save(book3);
 
+        DBHelper.save(user1);
+        DBHelper.save(user2);
+        DBHelper.save(user3);
     }
 
 }
