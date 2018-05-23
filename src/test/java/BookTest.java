dcf028cba39ba167943777ee5a4738e96696d957
Edward Fallon <eddie_fallon@hotmail.com>
2018-05-23 13:44:18 +0100

    setup test file for book

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,14 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class BookTest {
+
+    Book book1;
+
+    @Before
+    public void setUp() throws Exception {
+        book1 = new Book("Mr Men", true);
+    }
+
+
+}
